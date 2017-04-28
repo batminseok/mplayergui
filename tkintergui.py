3074a2d56e81412bf296bbaf54a9f0637ef0b7f6
batminseok <flo_somerfield@hotmail.co.uk>
2017-04-28 10:39:14 +0000

    nice n bash

--- tkintergui.py
+++ tkintergui.py
@@ -12,6 +12,7 @@ from PIL import Image
 import mutagen
 from mutagen.mp3 import *
 from mutagen.id3 import ID3
+import thread
 
 #subprocess.Popen("amixer set Digital 50%", shell = True) volume eventually 
 
@@ -28,29 +29,35 @@ for file in os.listdir("/home/pi/Music/Resources"):
 
         
 def createPlaylist():
-	global songlist 
-	shufflist = songlist
-	shuffle(shufflist)
-	f = open("playlist.m3u", "w")
-	for i in shufflist:
-		f.write("/home/pi/Music/Resources/" + i + "\n")
-	f.close()
-	
+    global songlist 
+    shufflist = songlist
+    shuffle(shufflist)
+    f = open("playlist.m3u", "w")
+    for i in shufflist:
+        f.write("/home/pi/Music/Resources/" + i + "\n")
+    f.close()
+    
 
 def getSong(): #which song is currently being played
-	global songNumber
-	with open("playlist.m3u") as fp:
-		for i, line in enumerate(fp):
-			if i == songNumber:
-				currentSong = line
-				print "current song(get song): " + str(i) 
-	formattedSong = currentSong.rstrip().replace('.mp3', '').replace('/home/pi/Music/Resources/', '')
-	print formattedSong
-	return formattedSong
-	
-	
+    global songNumber
+    with open("playlist.m3u") as fp:
+        for i, line in enumerate(fp):
+            if i == songNumber:
+                currentSong = line
+                print "current song(get song): " + str(i) 
+    formattedSong = currentSong.rstrip().replace('.mp3', '').replace('/home/pi/Music/Resources/', '')
+    print formattedSong
+    return formattedSong
+    
+def callmplayer():#really fuck
+    thread.start_new_thread(dumbworkaround, ())
+    
+def dumbworkaround():
+	os.system('mplayer -slave -ao alsa:device=hw=0,0 -playlist playlist.m3u')
 
-class SampleApp(tk.Tk):
+    
+
+class mp3App(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -119,7 +126,7 @@ class nowPlaying(tk.Frame):
         s.theme_use('alt')
         s.configure("TProgressbar", foreground = 'white', background = 'white', troughcolor = 'gray27', thickness = 5,)
         self.progressBar = ttk.Progressbar(self, style = "TProgressbar", orient = "horizontal", 
-		                       length = 350, mode = "determinate", maximum = 100)
+                               length = 350, mode = "determinate", maximum = 100)
         self.progressBar.place(relx = 0.5, rely = 0.55, anchor = CENTER)
         
         self.secv = StringVar()
@@ -134,178 +141,165 @@ class nowPlaying(tk.Frame):
         
         
         
-		
+        
         
     def backPress(self):
-		
-		k = PyKeyboard()
-		
-		if started == 1:
-			print "back button pressed"
-			k.press_keys([k.alt_key, k.tab_key])
-			time.sleep(0.5)
-			k.press_keys([k.shift_key, ','])
-			time.sleep(0.5)
-			k.press_keys([k.alt_key, k.tab_key])
-			
-			self.progressBar.stop()
-			self.after_cancel(self.count)
-			self.decreaseSongNumber()
-			self.secv.set('0:00')
-		
-		
+        
+        k = PyKeyboard()
+        
+        if started == 1:
+            print "back button pressed"
+            os.system("bash mplayerbash.sh key_down_event 60")
+            
+            self.progressBar.stop()
+            self.after_cancel(self.count)
+            self.decreaseSongNumber()
+            self.secv.set('0:00')
+        
+        
     def playPress(self):
-		
-		global started
-		global songNumber
-		k = PyKeyboard()
-		
-		if started == 0: #start tunes
-			print "started"
-			k.press_keys([k.alt_key, k.tab_key])
-			time.sleep(0.5)
-			k.type_string('mplayer -ao alsa:device=hw=0,0 -playlist playlist.m3u')
-			k.tap_key(k.keypad_keys['Enter'])
-			k.press_keys([k.alt_key, k.tab_key])
+        
+        global started
+        global songNumber
+        k = PyKeyboard()
+        
+        if started == 0: #start tunes
 			
-			createPlaylist()
-			
-			self.editInfo() #runs main looping through music loop
-			
-			started = 1
-			
-		elif started == 1: #song is playing, button pauses
-			global paused
-			print "playing"
-			k.press_keys([k.alt_key, k.tab_key])
-			time.sleep(0.1)
-			k.tap_key('p')
-			time.sleep(0.1)
-			k.press_keys([k.alt_key, k.tab_key])
-			if paused == False: #if playing
-				#change icon
-				#cancel after_timer 
-				self.after_cancel(self.count)
-				step_amount = ((1 - (self.seconds / self.length)) * 100)
-				print str(step_amount)
-				self.progressBar.stop()
-				print "paused"
-				self.progressBar.step(step_amount) #step is a %
-				paused = True
-			elif paused == True: #if paused
-				#change icon
-				self.timer()
-				self.progressBar.start(self.intervalLength)
-				print "playing"
-				paused = False
-				
-		
+            createPlaylist()
+            callmplayer()
+            
+            self.editInfo() #runs main looping through music loop
+            
+            started = 1
+            
+        elif started == 1: #song is playing, button pauses
+            global paused
+            print "playing"
+            
+            if paused == False: #if playing
+                os.system("bash mplayerbash.sh key_down_event 112")
+                print "bash called"
+                #change icon
+                #cancel after_timer 
+                self.after_cancel(self.count)
+                step_amount = ((1 - (self.seconds / self.length)) * 100)
+                print str(step_amount)
+                self.progressBar.stop()
+                print "paused"
+                self.progressBar.step(step_amount) #step is a %
+                paused = True
+            else:
+                os.system("bash mplayerbash.sh key_down_event 112")
+                print "bash called"
+                #change icon
+                self.timer()
+                self.progressBar.start(self.intervalLength)
+                print "playing"
+                paused = False
+                
+        
     def forwardPress(self):
-		
-		if started == 1:
-			
-			k = PyKeyboard()
-			print "forward button pressed"
-			k.press_keys([k.alt_key, k.tab_key])
-			time.sleep(0.5)
-			k.press_keys([k.shift_key, '.'])
-			time.sleep(0.5)
-			k.press_keys([k.alt_key, k.tab_key])
-			self.progressBar.stop()
-			self.after_cancel(self.count)
-			self.increaseSongNumber()
-			self.secv.set('0:00')
-			
-			
-				
+        
+        if started == 1:
+            
+            k = PyKeyboard()
+            print "forward button pressed"
+            os.system("bash mplayerbash.sh key_down_event 62")
+            self.progressBar.stop()
+            self.after_cancel(self.count)
+            self.increaseSongNumber()
+            self.secv.set('0:00')
+            
+            
+                
     def editInfo(self):
-		print "info edited"
-		global started
-		global paused
-		
-		song = getSong()# #get song name eg Just.mp3
-		pngstring = "/home/pi/Music/Albumart/" + song + ".png"
-		mp3string = "/home/pi/Music/Resources/" + song + ".mp3"
-		mp3 = MP3(mp3string)
-		
-		self.length = mp3.info.length
-		
-		self.intervalLength = int(self.length) * 10 #bars' currently a bit out
-		
-		title = mp3["TIT2"]
-		artist = mp3["TPE1"]
-		self.artistName.config(text = artist)
-		self.songTitle.config(text = title)
-		self.seconds = self.length
-		seconds = int(self.length%60)
-		minutes = int(self.length/60)
-		if seconds < 10:
-			self.lengthv.set(str(minutes) + ':0' + str(seconds))
-		else:
-			self.lengthv.set(str(minutes) + ':' + str(seconds))
-		
-		if started == 0:
-			self.seconds += 1
-		print self.seconds
-		
-		try:
-			photo2 = ImageTk.PhotoImage(file = pngstring)
-			self.switchbutton.config(image = photo2)
-			self.switchbutton.image = photo2
-		except:
-			print "srry"
-			#could put placeholder image here/or just sort art out 
-			
-		try:
-			self.after_cancel(self.count)
-		except:
-			print "caught"
-		
-		#if not paused, used to not start timer on skip events + avoid double timer: 
-		if paused == False:
-		    self.timer()
-		    self.progressBar.start(self.intervalLength)
-		
-		print "after started"
-		
-		
-		
+        print "info edited"
+        global started
+        global paused
+        
+        song = getSong()# #get song name eg Just.mp3
+        pngstring = "/home/pi/Music/Albumart/" + song + ".png"
+        mp3string = "/home/pi/Music/Resources/" + song + ".mp3"
+        mp3 = MP3(mp3string)
+        
+        self.length = mp3.info.length
+        
+        self.intervalLength = int(self.length) * 10 #bars' currently a bit out
+        
+        title = mp3["TIT2"]
+        artist = mp3["TPE1"]
+        self.artistName.config(text = artist)
+        self.songTitle.config(text = title)
+        self.seconds = self.length
+        seconds = int(self.length%60)
+        minutes = int(self.length/60)
+        if seconds < 10:
+            self.lengthv.set(str(minutes) + ':0' + str(seconds))
+        else:
+            self.lengthv.set(str(minutes) + ':' + str(seconds))
+        
+        if started == 0:
+            self.seconds += 1
+        print self.seconds
+        
+        try:
+            photo2 = ImageTk.PhotoImage(file = pngstring)
+            self.switchbutton.config(image = photo2)
+            self.switchbutton.image = photo2
+        except:
+            print "srry"
+            #could put placeholder image here/or just sort art out 
+            
+        try:
+            self.after_cancel(self.count)
+        except:
+            print "caught"
+        
+        #if not paused, used to not start timer on skip events + avoid double timer: 
+        if paused == False:
+            self.timer()
+            self.progressBar.start(self.intervalLength)
+        
+        print "after started"
+        
+        
+        
     def timer(self): #could be nice to display on the bar or something
-		
-		seconds = int((self.length - self.seconds)%60)
-		minutes = int((self.length - self.seconds)/60)
-		if seconds < 10:
-			self.secv.set(str(minutes) + ':0' + str(seconds))
-		else:
-			self.secv.set(str(minutes) + ':' + str(seconds))
-			
-			
-		self.seconds -= 1
-		print self.seconds
-		if self.seconds < 1:
-			print "timer finished"
-			self.increaseSongNumber()
-		else:
-			print "timertime"
-			self.count = self.after(1000, self.timer)
-		
-		
+        
+        seconds = int((self.length - self.seconds)%60)
+        minutes = int((self.length - self.seconds)/60)
+        if seconds < 10:
+            self.secv.set(str(minutes) + ':0' + str(seconds))
+        else:
+            self.secv.set(str(minutes) + ':' + str(seconds))
+            
+            
+        self.seconds -= 1
+        if self.seconds < 1:
+            print "timer finished"
+            self.increaseSongNumber()
+        else:
+            self.count = self.after(1000, self.timer)
+        
+        
     def increaseSongNumber(self):
-		global songNumber
-		self.after_cancel(self.count)
-		songNumber += 1
-		print "song number = " + str(songNumber)
-		self.editInfo()
-		
-		
+        global songNumber
+        self.after_cancel(self.count)
+        songNumber += 1
+        print "song number = " + str(songNumber)
+        self.editInfo()
+        
+        
     def decreaseSongNumber(self):
-		global songNumber
-		self.after_cancel(self.count)
-		songNumber -= 1
-		print "song number (decreased)= " + str(songNumber)
-		self.editInfo()
-		
-		
+        global songNumber
+        self.after_cancel(self.count)
+        songNumber -= 1
+        print "song number (decreased)= " + str(songNumber)
+        self.editInfo()
+        
+   	
+        
+        
 
 
 class songList(tk.Frame):
@@ -320,5 +314,5 @@ class songList(tk.Frame):
 
 
 if __name__ == "__main__":
-    app = SampleApp()
+    app = mp3App()
     app.mainloop()
